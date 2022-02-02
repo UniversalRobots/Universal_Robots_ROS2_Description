8d097dc337903b2f467f802759b6196d08ec237b
Lukas Sackewitz <sackewitz@fzi.de>
2022-02-02 14:57:17 +0100

    added license header, cleaned up imports

--- test/test_ur_urdf_xacro.py
+++ test/test_ur_urdf_xacro.py
@@ -1,3 +1,33 @@
+# Copyright (c) 2022 FZI Forschungszentrum Informatik
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the {copyright_holder} nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Lukas Sackewitz
+
 import os
 import shutil
 import subprocess
@@ -7,7 +37,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def test_ur_urdf_xacro():
-    
+
     # Initialize Arguments
     ur_type = "ur3"
     safety_limits = "true"
@@ -19,34 +49,29 @@ def test_ur_urdf_xacro():
     prefix = '""'
 
     joint_limit_params = os.path.join(
-        get_package_share_directory(description_package),
-        "config",
-        ur_type,
-        "joint_limits.yaml"
+        get_package_share_directory(description_package), "config", ur_type, "joint_limits.yaml"
     )
     kinematics_params = os.path.join(
         get_package_share_directory(description_package),
         "config",
         ur_type,
-        "default_kinematics.yaml"
+        "default_kinematics.yaml",
     )
     physical_params = os.path.join(
         get_package_share_directory(description_package),
         "config",
         ur_type,
-        "physical_parameters.yaml"
+        "physical_parameters.yaml",
     )
     visual_params = os.path.join(
         get_package_share_directory(description_package),
         "config",
         ur_type,
-        "visual_parameters.yaml"
+        "visual_parameters.yaml",
     )
 
     description_file_path = os.path.join(
-        get_package_share_directory(description_package),
-        "urdf",
-        description_file
+        get_package_share_directory(description_package), "urdf", description_file
     )
 
     (_, tmp_urdf_output_file) = tempfile.mkstemp(suffix=".urdf")
@@ -88,5 +113,5 @@ def test_ur_urdf_xacro():
         os.remove(tmp_urdf_output_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_ur_urdf_xacro()
