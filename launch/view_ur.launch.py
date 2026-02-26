9f11b404ddaf0c26273a1373ffd948b824a1cc8b
Felix Exner <feex@universal-robots.com>
2026-02-26 12:06:33 +0100

    Fixes

--- launch/view_ur.launch.py
+++ launch/view_ur.launch.py
@@ -54,12 +54,6 @@ def generate_launch_description():
                 ),
                 launch_arguments={
                     "ur_type": LaunchConfiguration("ur_type"),
-                    "rviz_config_file": LaunchConfiguration("rviz_config_file"),
-                    "description_file": LaunchConfiguration("description_file"),
-                    "tf_prefix": LaunchConfiguration("tf_prefix"),
-                    "safety_limits": LaunchConfiguration("safety_limits"),
-                    "safety_pos_margin": LaunchConfiguration("safety_pos_margin"),
-                    "safety_k_position": LaunchConfiguration("safety_k_position"),
                 }.items(),
             )
         ]
